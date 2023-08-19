38e1cda4a79f407b850c385f40ca5e5a9e04bd41
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-08-19 22:17:49 +0530

    Solution for question 2 and 3

--- case_study_1_dannys_dinner/queries.sql
+++ case_study_1_dannys_dinner/queries.sql
@@ -15,3 +15,43 @@ GROUP BY
 ORDER BY
 	s.customer_id ASC
 ;
+
+-- 2. How many days has each customer visited the restaurant?
+SELECT
+	s.customer_id,
+	COUNT(DISTINCT s.order_date) AS total_visits
+FROM
+	dannys_dinner.sales s
+GROUP BY
+	s.customer_id
+ORDER BY
+	s.customer_id ASC
+;
+
+-- 3. What was the first item from the menu purchased by each customer?
+SELECT
+	DISTINCT customer_id,
+	product_name
+FROM
+	(
+		SELECT
+			s.customer_id,
+			m.product_name,
+			DENSE_RANK() OVER (
+				PARTITION BY
+					s.customer_id
+				ORDER BY
+					s.order_date
+			) AS first_order
+		FROM
+			dannys_dinner.sales s
+		INNER JOIN
+			dannys_dinner.menu m
+		ON
+			s.product_id = m.product_id
+		ORDER BY
+			customer_id ASC
+	) rn
+WHERE
+	first_order = 1
+;
