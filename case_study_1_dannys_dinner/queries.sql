d2ff89cba500d4163d6cac6db69c7e88a073036e
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-08-20 21:54:51 +0530

    Added solution for questions 4 and 5

--- case_study_1_dannys_dinner/queries.sql
+++ case_study_1_dannys_dinner/queries.sql
@@ -55,3 +55,55 @@ FROM
 WHERE
 	first_order = 1
 ;
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT
+	m.product_name,
+	COUNT(m.product_name) AS total_purchases
+FROM
+	dannys_dinner.sales s
+INNER JOIN
+	dannys_dinner.menu m
+ON
+	s.product_id = m.product_id
+GROUP BY
+	m.product_name
+ORDER BY
+	total_purchases DESC
+LIMIT 1
+;
+
+-- 5. Which item was the most popular for each customer?
+WITH popular_items AS (
+	SELECT
+		s.customer_id,
+		m.product_name,
+		COUNT(m.product_id) AS most_purchased,
+		DENSE_RANK() OVER(
+			PARTITION BY
+				s.customer_id
+			ORDER BY
+				COUNT(s.customer_id) DESC
+		) AS most_purchased_rank
+	FROM
+		dannys_dinner.sales s
+	INNER JOIN
+		dannys_dinner.menu m
+	ON
+		s.product_id = m.product_id
+	GROUP BY
+		1, 2
+	ORDER BY
+		1 ASC, 3 DESC
+)
+
+SELECT
+	customer_id,
+	product_name
+FROM
+	popular_items
+WHERE
+	most_purchased_rank = 1
+;
+
+-- 6. Which item was purchased first by the customer after they became a member?
